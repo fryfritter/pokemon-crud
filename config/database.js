657ebc9b1b76c08c053fb708e115b1869cc9326a
fryfritter <ryan.devqr@gmail.com>
2021-08-25 14:34:23 +0800

    initial commit with sequelize basic setup

---
+++ config/database.js
@@ -0,0 +1,26 @@
+// config/database.js
+
+module.exports = {
+  development: {
+    username: "devtraining",
+    password: null,
+    database: "devtraining",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "devtraining",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres",
+  },
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+const sequelize = require("./db/index.js");
+
+try {
+  sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
